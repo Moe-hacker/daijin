82e7789c08141a61a9fbc9219f542a6b5416e098
Moe-hacker <moe-hacker@outlook.com>
2021-08-20 10:25:33 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ mkdir -p /data/data/com.termux/files/usr/share/termux-container
 git clone https://github.com/Moe-hacker/termux-container /data/data/com.termux/files/usr/share/termux-container
 cd /data/data/com.termux/files/usr/share/termux-container
 cp container /data/data/com.termux/files/usr/bin/
-container configure
+bash /data/data/com.termux/files/usr/bin/container configure
