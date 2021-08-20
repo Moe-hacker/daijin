35a41e5cdd536ab918d08521c5706088ca2e30e1
Moe-hacker <moe-hacker@outlook.com>
2021-08-20 10:08:19 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #########用于安装此项目脚本#########
 #!/data/data/com.termux/files/usr/bin/bash
 pkg install git wget tsu curl e2fsprogs
-git clone https://github.com/Moe-hacker/termux-container /data/data/com.termux/files/usr/share
+mkdir -p /data/data/com.termux/files/usr/share/termux-container
+git clone https://github.com/Moe-hacker/termux-container /data/data/com.termux/files/usr/share/termux-container
 cd /data/data/com.termux/files/usr/share/termux-container
 cp container /data/data/com.termux/files/usr/bin/
 container configure
