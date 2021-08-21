cb11ee6d5a846fc5aa4ebb3f0d574e7b0e8ec762
Moe-hacker <shichengniu2@gmail.com>
2021-08-21 11:41:01 +0800

    …………

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
 #########用于安装此项目脚本#########
-echo -e "\033[34m***********************************************"
+echo -e "\033[34m***********************************************\033[0m"
 echo -e "\033[34m*********\033[32mtermux-containe by Moe-hacker\033[34m*********\033[0m"
 echo -e "\033[34m*********\033[31m        NO WARRANTY          \033[34m*********\033[0m"
 echo -e "\033[34m*********\033[32m   Press Enter to continue   \033[34m*********\033[0m"
-echo -e "\033[34m***********************************************"
+echo -e "\033[34m***********************************************\033[0m"
 read
 pkg install git wget tsu curl e2fsprogs
 mkdir -p /data/data/com.termux/files/usr/share/termux-container
