5254b6e4b0ea9f57a30c31ec932d74275f803e3d
Moe-hacker <shichengniu2@gmail.com>
2021-08-21 23:30:41 +0800

    …………

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 #########用于安装此项目脚本#########
+clear
 echo -e "\033[31m***********************************************\033[0m"
 echo -e "\033[31m*********\033[31mtermux-containe by Moe-hacker\033[31m*********\033[0m"
 echo -e "\033[31m*********\033[31m        NO WARRANTY          \033[31m*********\033[0m"
@@ -11,4 +12,4 @@ mkdir -p /data/data/com.termux/files/usr/share/termux-container
 git clone https://github.com/Moe-hacker/termux-container /data/data/com.termux/files/usr/share/termux-container
 cd /data/data/com.termux/files/usr/share/termux-container
 cp container /data/data/com.termux/files/usr/bin/
-echo -e "\033[31mPlease run container configure\033[0m"
+echo -e "\033[31mPLEASE RUN container configure\033[0m"
