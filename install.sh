ddf59b866d0d5d883751d094d9308600fba04d3a
Moe-hacker <shichengniu2@gmail.com>
2021-08-21 14:34:59 +0800

    …………

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/data/data/com.termux/files/usr/bin/bash
 #########用于安装此项目脚本#########
-echo -e "\033[34m***********************************************\033[0m"
-echo -e "\033[34m*********\033[32mtermux-containe by Moe-hacker\033[34m*********\033[0m"
-echo -e "\033[34m*********\033[31m        NO WARRANTY          \033[34m*********\033[0m"
-echo -e "\033[34m*********\033[32m   Press Enter to continue   \033[34m*********\033[0m"
-echo -e "\033[34m***********************************************\033[0m"
+echo -e "\033[31m***********************************************\033[0m"
+echo -e "\033[31m*********\033[31mtermux-containe by Moe-hacker\033[31m*********\033[0m"
+echo -e "\033[31m*********\033[31m        NO WARRANTY          \033[31m*********\033[0m"
+echo -e "\033[31m*********\033[31m   Press Enter to continue   \033[31m*********\033[0m"
+echo -e "\033[31m***********************************************"
 read
 pkg install git wget tsu curl e2fsprogs
 mkdir -p /data/data/com.termux/files/usr/share/termux-container
 git clone https://github.com/Moe-hacker/termux-container /data/data/com.termux/files/usr/share/termux-container
 cd /data/data/com.termux/files/usr/share/termux-container
 cp container /data/data/com.termux/files/usr/bin/
-echo -e "\033[34mPlease run \033[33mcontainer configure\033[0m"
+echo -e "\033[31mPlease run container configure\033[0m"
