88756a4055b744d9d4b5939e1e70e3943c528f06
Moe-hacker <moe-hacker@outlook.com>
2021-11-06 15:02:11 +0000

    .

--- update.sh
+++
@@ -1,8 +0,0 @@
-if [[ ! -e /data/data/com.termux/files/usr/etc/container.conf.d ]];then
-  mkdir /data/data/com.termux/files/usr/etc/container.conf.d
-  echo NAME=Default >> /data/data/com.termux/files/usr/etc/container.conf
-  echo CUSOR=block >> /data/data/com.termux/files/usr/etc/container.conf
-  mv /data/data/com.termux/files/usr/etc/container.conf /data/data/com.termux/files/usr/etc/container.conf.d/container-1.conf
-  echo CONTAINER=1 >>/data/data/com.termux/files/usr/etc/container.conf
-  touch /data/data/com.termux/files/usr/share/termux-container/.updated
-fi
